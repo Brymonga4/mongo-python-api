9343cbb378ef1eea2421335508b389bc850a9577
Brymonga4 <bryanmoises.montesdeoca@alumno.iesluisvives.org>
2024-04-22 12:23:34 +0200

    Postgresql needs some tweaking

--- api/src/hello-world.py
+++ api/src/hello-world.py
@@ -3,10 +3,22 @@ from pymongo import MongoClient
 print('Hello World!')
 
 client = MongoClient("mongodb://root:example@mongo:27017")
+
+if client:
+    print("Conexión exitosa")
+
 db = client.testdb
 
-try: db.command("serverStatus")
-except Exception as e: print(e)
-else: print("You are connected!")
+collection= db.collectiontest
+documento = {"nombre": "ejemplo"}
+collection.insert_one(documento)
+
+print("Número total de documentos:", collection.count_documents({}))
+result = collection.find()
+
+print("Antes del bucle")
+for document in result:
+    print(document)
+print("Después del bucle")
 
 client.close()
